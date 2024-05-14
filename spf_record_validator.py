2c6231e32024144e5abcd5c810b473d19ad0f071
kerasjan <mateusz.najsarek@gmail.com>
2024-05-14 23:51:16 +0200

    Probably testing ready
    Add support for checking ipv6, a, mx, ptr and exists mechanisms add comments

--- spf_record_validator.py
+++ spf_record_validator.py
@@ -1,15 +1,19 @@
 import dns.resolver
 import re
 
-#Helping functions
+
+# Helping functions
 def throw_issue(severity: str, message: str, issues: list):
     issues.append({"severity": severity, "message": message})
 
-def is_ip4_public(ip):
 
-    #Check if it is an ip
+def is_public_ip4(ip: str):
 
-    ip_regex = re.compile(r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(/(3[0-2]|[1-2][0-9]|[1-9]))?$")
+    # Check if it is an ipv4 address
+
+    ip_regex = re.compile(
+        r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(/(3[0-2]|[1-2][0-9]|[1-9]))?$"
+    )
 
     if not ip_regex.match(ip):
         return False
@@ -28,30 +32,76 @@ def is_ip4_public(ip):
     return True
 
 
+def is_ip6_valid(ip: str):
+    # Check if it is an ipv6 address
+    # helper variables
+    double_colon = False
+    omitting_zeros = False
+    ip_regex = re.compile(r"[0-9a-f:]+(/(1[0-2][0-8]|[1-9][0-9]|[1-9]))?$")
+
+    # Initial check of the syntax (characters and prefix length)
+    if ip_regex.match(ip):
+        ip = ip.split("/")[0]
+        segments = ip.split(":")
+        for index, segment in enumerate(segments):
+            # Check for the zeros compression
+            if segment == "":
+                if double_colon:
+                    if index != len(segments) - 1:
+                        return False
+                double_colon = True
+
+            # Check the syntax for each segment
+            elif re.match(r"^[0-9a-f]{1-4}$", segment):
+                if len(segment) < 4:
+                    omitting_zeros = True
+                if omitting_zeros and segment.startswith("0"):
+                    return False
+
+        # TO DO Uni/Multi/Any cast check if it changes anything
+
+        return True
+    return False
+
+
 def find_spf_record(domain: str, issues: list):
     # Find the SPF record for the provided domain
     try:
-        records = dns.resolver.resolve(domain, 'TXT')
+        records = dns.resolver.resolve(domain, "TXT")
         for record in records:
             record = str(record)
             if record.startswith("v=spf1"):
-                validate_spf(record, issues)
+                validate_spf(record, domain, issues)
     except:
         throw_issue("critical", "No SPF record found", issues)
+
 
 def find_a_record(domain: str, issues: list):
     # Find the A record for the provided domain
     a_redords = []
     try:
-        records = dns.resolver.resolve(domain, 'A')
+        records = dns.resolver.resolve(domain, "A")
         for record in records:
             record = str(record)
             a_redords.append(record)
-        return a_redords
     except:
         throw_issue("critical", "No A record found", issues)
+    return a_redords
 
-def validate_spf(spf_record: str, issues: list = []):
+
+def find_mx_record(domain: str, issues: list):
+    mx_records = []
+    try:
+        records = dns.resolver.resolve(domain, "MX")
+        for record in records:
+            record = str(record)
+            mx_records.append(record)
+    except:
+        throw_issue("critical", "No MX record found", issues)
+    return mx_records
+
+
+def validate_spf(spf_record: str, curr_domain: str, issues: list = []):
     # Check if the SPF record is valid
 
     # helper variables
@@ -59,9 +109,15 @@ def validate_spf(spf_record: str, issues: list = []):
     redirect_check = False
     issues = []
     test_status = True
-    
+
     # Check if it is an spf record
     if spf_record.startswith("v=spf1"):
+        if re.match(r"[A-Z]", spf_record):
+            throw_issue(
+                "critical",
+                "Spf record should must not contain uppercase characters in it",
+                issues,
+            )
         # Split the record into items
         spf_record = spf_record.split(" ")
         spf_record.pop(0)
@@ -70,55 +126,188 @@ def validate_spf(spf_record: str, issues: list = []):
             # Check if the item is the all mechanism
             if item.endswith("all"):
                 if redirect_check:
-                    throw_issue("critical", "all mechanism is used with redirect mechanism which should not be done", issues)
+                    throw_issue(
+                        "critical",
+                        "all mechanism is used with redirect mechanism which should not be done",
+                        issues,
+                    )
                 if all_check:
-                    throw_issue("critical", "all mechanism is used more than once", issues)
+                    throw_issue(
+                        "critical", "all mechanism is used more than once", issues
+                    )
                 if len(item) != 4:
-                    throw_issue("critical", "all mechanism syntax must be [-+?~]all", issues)
+                    throw_issue(
+                        "critical", "all mechanism syntax must be [-+?~]all", issues
+                    )
                 if item.startswith("+") or item.startswith("?"):
-                    throw_issue("warning", "all mechanism should be prefixed with - or ~", issues)
+                    throw_issue(
+                        "warning",
+                        "all mechanism should be prefixed with - or ~",
+                        issues,
+                    )
                 all_check = True
             # Check if the item is the include mechanism
             elif item.startswith("include:"):
                 include_domain = item.split(":")[1]
                 find_spf_record(include_domain, issues)
 
-            # Check if the item is the redirect mechanism    
+            # Check if the item is the redirect mechanism
             elif item.startswith("redirect="):
                 if redirect_check:
-                    throw_issue("critical", "redirect mechanism is used more than once", issues)
+                    throw_issue(
+                        "critical", "redirect mechanism is used more than once", issues
+                    )
                 if all_check:
-                    throw_issue("critical", "redirect mechanism is used after all mechanism", issues)
+                    throw_issue(
+                        "critical",
+                        "redirect mechanism should not be used after all mechanism - then all mechanism is not evaluated",
+                        issues,
+                    )
                 redirect_domain = item.split("=")[1]
                 find_spf_record(redirect_domain, issues)
                 redirect_check = True
 
+            # Check if the item is the ip4 mechanism
             elif item.startswith("ip4:"):
                 ip = item.split(":")[1]
-                if not is_ip4_public(ip):
-                    throw_issue("warning", f"The ip address: {ip} is not a public ipv4 address", issues)
-            
-            #TO DO: Add support for ipv6 mechanism
-            elif item.startswith("ip6:"):
-                #ipv6 not yet supported
-                throw_issue("low", "IPv6 is not yet supported", issues)
+                if not is_public_ip4(ip):
+                    throw_issue(
+                        "warning",
+                        f"The ip address: {ip} is not a public ipv4 address",
+                        issues,
+                    )
 
+            # Check if the item is the ip6 mechanism
+            elif item.startswith("ip6:"):
+                ip = item.split(":")[1]
+                if not is_ip6_valid(ip):
+                    throw_issue("critical", f"Invalid ipv6 address: {ip}", issues)
+
+            # Check if the item is the a mechanism
             elif item.startswith("a"):
-                #TO DO: Add support for a with no arguments
+                # TO DO: Add support for a with no arguments
                 if item == "a":
-                    throw_issue("low", "a with no arguments is not yet supported", issues)
-                if item.startswith("a:"):
-                    domain = item.split(":")[1]
-                    a_records = find_a_record(domain, issues)
+                    a_records = find_a_record(curr_domain, issues)
                     for record in a_records:
-                        if not is_ip4_public(record):
-                            throw_issue("warning", f"The ip address: {record} from A mechanism is not a public ipv4 address", issues)
-                #TO DO: Add support for a/ mechanism
+                        if not is_public_ip4(record):
+                            throw_issue(
+                                "warning",
+                                f"The ip address: {record} from the A record of the domain: {curr_domain} is not a public ipv4 address",
+                                issues,
+                            )
+                elif item.startswith("a:"):
+                    domain = item.split(":")[1]
+                    if len(domain.split("/")) > 2:
+                        throw_issue(
+                            "error",
+                            f"Invalid value:{domain} for the A mechanism, it should be a[:domain][/prefix]",
+                            issues,
+                        )
+                    elif len(domain.split("/")) > 1 and not re.match(
+                        r"^(/(3[0-2]|[1-2][0-9]|[1-9]))$", domain.split("/")[1]
+                    ):
+                        throw_issue(
+                            "error", "Invalid prefix length in the A mechanism", issues
+                        )
+                    else:
+                        domain = domain.split("/")[0]
+
+                        a_records = find_a_record(domain, issues)
+                        for record in a_records:
+                            if not is_public_ip4(record):
+                                throw_issue(
+                                    "warning",
+                                    f"The ip address: {record} from the A record of the provided domain: {domain} is not a public ipv4 address",
+                                    issues,
+                                )
+                # Not sure how to know if there would be an ipv4 or ipv6 address though it is easily rewritable to support both
                 elif item.startswith("a/"):
-                    throw_issue("low", "a/ mechanism is not yet supported", issues)
+                    prefix = item.split("/")[1]
+                    if not re.match(r"^(/(3[0-2]|[1-2][0-9]|[1-9]))$", prefix):
+                        throw_issue(
+                            "error", "Invalid prefix length in the A mechanism", issues
+                        )
+                    a_records = find_a_record(curr_domain, issues)
+                    for record in a_records:
+                        if not is_public_ip4(record):
+                            throw_issue(
+                                "warning",
+                                f"The ip address: {record} from the A record of the current domain: {curr_domain} is not a public ipv4 address",
+                                issues,
+                            )
+
+            #  Check if the item is the mx mechanism
+            elif item.startswith("mx"):
+                if item == "mx":
+                    mx_records = find_mx_record(curr_domain, issues)
+                    for mx_record in mx_records:
+                        a_records = find_a_record(mx_record.split(" ")[1], issues)
+                        for a_record in a_records:
+                            if not is_public_ip4(a_record):
+                                throw_issue(
+                                    "warning",
+                                    f"The ip address: {a_record} from the MX record of the current domain: {curr_domain} is not a public ipv4 address",
+                                    issues,
+                                )
+                elif item.startswith("mx:"):
+                    domain = item.split(":")[1]
+                    if len(domain.split("/")) > 2:
+                        throw_issue(
+                            "error",
+                            f"Invalid value:{domain} for the A mechanism, it should be a[:domain][/prefix]",
+                            issues,
+                        )
+                    elif len(domain.split("/")) > 1 and not re.match(
+                        r"^(/(3[0-2]|[1-2][0-9]|[1-9]))$", domain.split("/")[1]
+                    ):
+                        throw_issue(
+                            "error", "Invalid prefix length in the A mechanism", issues
+                        )
+                    else:
+                        domain = domain.split("/")[0]
+                        mx_records = find_mx_record(domain, issues)
+                        for mx_record in mx_records:
+                            a_records = find_a_record(mx_record.split(" ")[1], issues)
+                            for a_record in a_records:
+                                if not is_public_ip4(a_record):
+                                    throw_issue(
+                                        "warning",
+                                        f"The ip address: {record} from the MX record of the provided domain: {domain} is not a public ipv4 address",
+                                        issues,
+                                    )
+                elif item.startswith("mx/"):
+                    prefix = item.split("/")[1]
+                    if not re.match(r"^(/(3[0-2]|[1-2][0-9]|[1-9]))$", prefix):
+                        throw_issue(
+                            "error", "Invalid prefix length in the A mechanism", issues
+                        )
+                    mx_records = find_mx_record(curr_domain, issues)
+                    for mx_record in mx_records:
+                        a_records = find_a_record(mx_record.split(" ")[1], issues)
+                        for a_record in a_records:
+                            if not is_public_ip4(a_record):
+                                throw_issue(
+                                    "warning",
+                                    f"The ip address: {record} from the MX record of the current domain: {curr_domain} is not a public ipv4 address",
+                                    issues,
+                                )
+
+            # Check if the item is the exists mechanism
+            elif item.startswith("exists:"):
+                domain = item.split(":")[1]
+                if not len(find_a_record(domain, issues)) > 0:
+                    throw_issue(
+                        "warning",
+                        f"Domain provided in exists mehcanism: {domain} is not a match or is not valid",
+                        issues,
+                    )
+
+            # Check if the item is the ptr mechanism
+            elif item.startswith("ptr"):
+                throw_issue("warning", "ptr mechanism should not be published", issues)
 
             else:
-                throw_issue("critical", f"Unknown mechanism: {item}", issues)
+                throw_issue("critical", f"Invalid mechanism: {item}", issues)
 
     else:
         throw_issue("critical", "Provided string is not an SPF record", issues)
@@ -127,10 +316,11 @@ def validate_spf(spf_record: str, issues: list = []):
         test_status = False
     return {"status": test_status, "issues": issues}
 
-valid_spf = "v=spf1 a:group-ib.ru include:_spf.google.com ?all redirect:google.com"
 
-result = validate_spf(valid_spf)
+valid_spf = "v=spf1 include:_spf.ikea.com include:spf.protection.outlook.com -all"
 
-print(result['status'])
-for issue in result['issues']:
+result = validate_spf(valid_spf, "ikea.com")
+
+print(result["status"])
+for issue in result["issues"]:
     print(issue)
